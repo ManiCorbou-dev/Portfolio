74f8ecf7959c3eabc73afe6a2f9bd8c55ee5fe40
Mani <mani.corbou.pro@gmail.com>
2026-06-24 00:06:50 +0200

    Ajout des fichiers du portfolio

---
+++ portfolio.js
@@ -0,0 +1,38 @@
+
+const header = document.getElementById("main-header");
+ 
+window.addEventListener("scroll", () => {
+ 
+    if (window.scrollY > 80) {
+    
+        header.classList.add("scrolled");
+    } else {
+        
+        header.classList.remove("scrolled");
+    }
+ 
+});
+
+
+// On récupère tous les boutons et tous les panneaux
+const tabBtns = document.querySelectorAll(".tab-btn");
+const tabPanels = document.querySelectorAll(".tab-panel");
+
+// Pour chaque bouton, on écoute le clic
+tabBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+
+    // 1. On retire "active" de tous les boutons et panneaux
+    tabBtns.forEach(b => b.classList.remove("active"));
+    tabPanels.forEach(p => p.classList.remove("active"));
+
+    // 2. On active le bouton cliqué
+    btn.classList.add("active");
+
+    // 3. On affiche le panneau qui correspond (via data-target)
+    const cible = btn.dataset.target; // ex: "apropos"
+    document.getElementById(cible).classList.add("active");
+
+  });
+});
+ 
